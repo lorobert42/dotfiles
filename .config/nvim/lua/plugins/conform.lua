6d601afdd0c03eda2976e0305632c5b75261cbb8
lorobert <lorobert@student.42lausanne.ch>
2024-05-12 11:28:05 +0200

    Python formatter & linter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
+  event = { 'BufWritePre', 'BufNewFile' },
   cmd = { 'ConformInfo' },
   keys = {
     {
@@ -15,6 +15,7 @@ return {
   opts = {
     formatters_by_ft = {
       rust = { 'rustfmt' },
+      python = { 'isort', 'black' },
     },
   },
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,38 @@
-local Plugin = {'nvim-lualine/lualine.nvim'}
-
-Plugin.event = 'VeryLazy'
-
--- See :help lualine.txt
-Plugin.opts = {
-	options = {
-		theme = 'catppuccin',
-		icons_enabled = false,
-		component_separators = '|',
-		section_separators = '',
-		disabled_filetypes = {
-			statusline = {'NvimTree'}
-		}
-	},
-}
-
-function Plugin.init()
-	vim.opt.showmode = false
+local function get_linters()
+  local linters = require('lint').get_running()
+  if #linters == 0 then
+    return "󰦕"
+  end
+  return "󱉶 " .. table.concat(linters, ", ")
 end
 
-return Plugin
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  opts = {
+    options = {
+      theme = 'catppuccin',
+      icons_enabled = false,
+      component_separators = '|',
+      section_separators = '',
+      disabled_filetypes = {
+        statusline = {'NvimTree'}
+      },
+    },
+    sections = {
+      lualine_y = {get_linters},
+    },
+    inactive_sections = {
+      lualine_y = {'progress'},
+    },
+    extensions = {
+      'fzf',
+      'lazy',
+      'man',
+      'mason',
+      'nvim-tree',
+      'toggleterm',
+    },
+  },
+}
+
